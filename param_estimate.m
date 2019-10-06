7af0b4413368e50f658fef008a8386ce17ad3ee1
Anindya Harchowdhury <harchowdhuryanindya2@gmail.com>
2019-10-06 22:05:39 +0530

    Update param_estimate.m

--- param_estimate.m
+++ param_estimate.m
@@ -5,7 +5,7 @@
 % Latest Modified : 03.02.2019
 % NLP optimization to find out the optimal value of $\alpha$, $\beta$ and
 % $d$ for calibrating the sensor while the mirrors are uplifted
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%pa%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % lsqnonlin
 % hold off; clear; close all
 % clear laser_pitch x_loc y_loc z_loc X Y Z;
@@ -14,33 +14,42 @@
 % pitch_off = 3.4;
 
 
-pitch_off = 0.0;
+
 min_ang = -2.08007788658;
 theta_res = 0.00613592332229;
-val = 3652;
-filename = 'scan_s1.txt';
+val = 2924;
+filename = '/home/anindya/paper_1_m_files_new/sensor_calibration/range_cal_8.txt';
 matR = csvread(filename, 1, 11);
 laser_time_inc = 97656.2514552 ;  % time resolution
 
-T1 = readtable('scan_s1.txt', 'ReadVariableNames', false);
+T1 = readtable('/home/anindya/paper_1_m_files_new/sensor_calibration/range_cal_8.txt', 'ReadVariableNames', false);
 start = 2;
-for i= start : 657
-
+for i= start : 528
+    
     a = T1{i,3};
-%     b= cellstr(a);
-%     c = char(b);
-   e =  str2double(a);
+    %     b= cellstr(a);
+    %     c = char(b);
+    e =  str2double(a);
     f1(1,i-1) = e;    % laser stamp
 end
 
-% % % for XM430_W210 robot
+% % for XM430_W210 robot
+
+% for i= 1 : 500
+%     x_loc(i,1:682) = matR(i,1:682) .* cos((1:682) * theta_res + min_ang );
+%     y_loc(i,1:682) = matR(i,1:682) .* sin((1:682) * theta_res + min_ang );
+% %     scatter(x_loc(i, 1:682), y_loc(1,1:682));
+% %     hold on
+% %     z_loc(l,i) = Z0;
+% end
 
 
-T2 = readtable('ser_s1.txt', 'ReadVariableNames', false);
+T2 = readtable('/home/anindya/paper_1_m_files_new/sensor_calibration/ser_cal_8.txt', 'ReadVariableNames', false);
 for i= start : val
     a1(1,i-1) = T2{i,3};
     e1 = str2double(a1(1,i-1));
     f2(1,i-1) = e1;     % servo stamp
+    
 end
 
 for i= start : val
@@ -49,9 +58,10 @@ for i= start : val
     f3(1,i-1) = e1;     % servo position
 end
 
-T4 = readtable('vicon_s1.txt', 'ReadVariableNames', false);  % laptop pose
+% -----------vicon data extract----------
+T4 = readtable('/home/anindya/paper_1_m_files_new/sensor_calibration/robo_pose_8.txt', 'ReadVariableNames', false);  % laptop pose
 
-len =12672;
+len =11020;
 for i= start : len
     a4(1,i-1) = T4{i,3};
     e4 = str2double(a4(1,i-1));
@@ -75,29 +85,30 @@ for i= start : len
     e7 = str2double(a7(1,i-1));
     f7(1,i-1) = e7;     % robot position -- z axis
 end
+% ------------------------------end-------------------------------
 
-
-filename = 'ser_s1.txt';
+filename = '/home/anindya/paper_1_m_files_new/sensor_calibration/ser_cal_8.txt';
 matA = csvread(filename, 1, 12);
 matB = matA(:, 1) * 0.088 * pi/180;
 length=1:size(matB);
 [m, n]=size(matA);
 % % len=601;
-frame_num = 656;
+frame_num = 528;
 laser_time_inc = 97656.2514552;  % time resolution
-for l=2:frame_num-1
+for l=1:frame_num-1
     laser_time(l,1:678) =  f1(l) + laser_time_inc*(0:677);
 end
 % Z0=0.1191;  % sensor height offset
 Z0=0.1125;
 % Servo pitch interpolation
-theta_var = 0.0;
+theta_var = 0.01;
 % theta_var = 0;
-pitch_correction = 0.015;
+pitch_correction = 0.017;
+pitch_off = 0.00;
 
 % laser stamp, servo stamp and robot pose synchronisation using VICON data
 count = 1;
-for l=2:654
+for l=2:528
     if l==340
         aa=1;
     end
@@ -139,7 +150,7 @@ for l=2:654
                         break;
                     end
                 end
-                count = count + 1;                
+                count = count + 1;
             elseif count >1 && count<=len && laser_time(l,i) < f4(count) && laser_time(l,i) < f4(count-1)
                 i3 = i;
                 if i3>1
@@ -200,8 +211,7 @@ end
 
 
 count = 1;
-
-for l=2:654
+for l=1:900
     i=1; c=1;
     while i~= 678
         if count <=val && laser_time(l,i) >= f2(count) && laser_time(l,i) <= f2(count+1)
@@ -238,13 +248,13 @@ for l=2:654
                     %                     X(l,i) = V(1) + robot_pose_x(l,i) + 0.07;
                     %                     Y(l,i) = V(2) + robot_pose_y(l,i);
                     
-                    X(l,i) = V(1) + 0.07 - servo_y(l,i);
-                    Y(l,i) = V(2) - servo_x(l,i);
-                    Z(l,i) = V(3) + 0.2555 + servo_z(l,i);
+                    %                     X(l,i) = V(1) + 0.07 - servo_y(l,i);
+                    %                     Y(l,i) = V(2) - servo_x(l,i);
+                    %                     Z(l,i) = V(3) + 0.2555 + servo_z(l,i);
                     
-%                     X(l,i) = V(1) + 0.07 ;
-%                     Y(l,i) = V(2) ;
-%                     Z(l,i) = V(3) + 0.2555 ;
+                    X(l,i) = V(1) + 0.07 ;
+                    Y(l,i) = V(2) ;
+                    Z(l,i) = V(3) + 0.2555 ;
                     i=i+1;
                     %                     c=c+1;
                     if i==678
@@ -286,13 +296,13 @@ for l=2:654
                     V =  TF * [x_loc(l,i) y_loc(l,i) z_loc(l,i)]';
                     %                     X(l,i) = V(1) + robot_pose_x(l,i) + 0.07;
                     %                     Y(l,i) = V(2) + robot_pose_y(l,i);
-                    X(l,i) = V(1) + 0.07 - servo_y(l,i);
-                    Y(l,i) = V(2) - servo_x(l,i);
-                    Z(l,i) = V(3) + 0.2555 + servo_z(l,i);
-
-%                     X(l,i) = V(1) + 0.07;
-%                     Y(l,i) = V(2);
-%                     Z(l,i) = V(3) + 0.2555 ;
+                    %                     X(l,i) = V(1) + 0.07 - servo_y(l,i);
+                    %                     Y(l,i) = V(2) - servo_x(l,i);
+                    %                     Z(l,i) = V(3) + 0.2555 + servo_z(l,i);
+                    
+                    X(l,i) = V(1) + 0.07;
+                    Y(l,i) = V(2);
+                    Z(l,i) = V(3) + 0.2555 ;
                     i=i+1;
                     %                     c= c+1;
                     if i==678
@@ -327,80 +337,86 @@ for l=2:654
     end
 end
 
-
-% for i =1:499
-%     B(i) = nnz(X(i,:));
-% end
 figure
-for i=2:654
+for i=2:900
     scatter3(100*X(i,256:450), 100*Y(i,256:450), 100*Z(i,256:450),1, 'b','o');
     hold on
 end
-% 
+
+axis([190 215 -150 200 -5 160])
+% for i =1:499
+%     B(i) = nnz(X(i,:));
+% end
+% figure
+% for i=2:900
+%     scatter3(100*X(i,256:450), 100*Y(i,256:450), 100*Z(i,256:450),1, 'b','o');
+%     hold on
+% end
+%
 % % figure
 % % for i=1:700
 % %     scatter3(100*X(i,1:677), 100*Y(i,1:677), 100*Z(i,1:677),1, 'b','o');
 % %     hold on
 % % end
-% 
+%
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % %  Plane fitting
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% 
-% c=1;
-% for i=1:399
-%     for j=226:450
-%         if X(i,j)>1.51
-%             M1(c,1)=X(i,j);
-%             M1(c,2)=Y(i,j);
-%             M1(c,3)=Z(i,j);
-%             c=c+1;
-%         end
-%     end
-% end
-% 
-% hold on
-% [coeff,score,roots] = pca(M1);
-% basis = coeff(:,1:2);
-% 
-% normal = coeff(:,3);
-% 
-% pctExplained = roots' ./ sum(roots);
-% 
-% [n,p] = size(M1);
-% meanX = mean(M1,1);
-% Xfit = repmat(meanX,n,1) + score(:,1:2)*coeff(:,1:2)';
-% residuals = M1 - Xfit;
-% 
-% error = abs((M1 - repmat(meanX,n,1))*normal);
-% sse = sum(error.^2);
-% 
-% [xgrid,ygrid] = meshgrid(linspace(min(M1(:,1)),max(M1(:,1)),5), ...
-%     linspace(min(M1(:,2)),max(M1(:,2)),5));
-% zgrid = (1/normal(3)) .* (meanX*normal - (xgrid.*normal(1) + ygrid.*normal(2)));
+
+
+c=1;
+for i=1:399
+    for j=226:450
+        if X(i,j)>1.51
+            M1(c,1)=X(i,j);
+            M1(c,2)=Y(i,j);
+            M1(c,3)=Z(i,j);
+            c=c+1;
+        end
+    end
+end
+
+hold on
+[coeff,score,roots] = pca(M1);
+basis = coeff(:,1:2);
+
+normal = coeff(:,3);
+
+pctExplained = roots' ./ sum(roots);
+
+[n,p] = size(M1);
+meanX = mean(M1,1);
+Xfit = repmat(meanX,n,1) + score(:,1:2)*coeff(:,1:2)';
+residuals = M1 - Xfit;
+
+error = abs((M1 - repmat(meanX,n,1))*normal);
+sse = sum(error.^2);
+
+[xgrid,ygrid] = meshgrid(linspace(min(M1(:,1)),max(M1(:,1)),5), ...
+    linspace(min(M1(:,2)),max(M1(:,2)),5));
+zgrid = (1/normal(3)) .* (meanX*normal - (xgrid.*normal(1) + ygrid.*normal(2)));
 % h = mesh(xgrid,ygrid,zgrid,'EdgeColor',[0 0 0],'FaceAlpha',0);
-% 
+
 % hold on
-% above = (M1-repmat(meanX,n,1))*normal < 0;
-% below = ~above;
-% nabove = sum(above);
-% X1 = [M1(above,1) Xfit(above,1) nan*ones(nabove,1)];
-% X2 = [M1(above,2) Xfit(above,2) nan*ones(nabove,1)];
-% X3 = [M1(above,3) Xfit(above,3) nan*ones(nabove,1)];
+above = (M1-repmat(meanX,n,1))*normal < 0;
+below = ~above;
+nabove = sum(above);
+X1 = [M1(above,1) Xfit(above,1) nan*ones(nabove,1)];
+X2 = [M1(above,2) Xfit(above,2) nan*ones(nabove,1)];
+X3 = [M1(above,3) Xfit(above,3) nan*ones(nabove,1)];
 % plot3(X1',X2',X3','-', M1(above,1),M1(above,2),M1(above,3),'o', 'MarkerSize', 1, 'Color',[0 .7 0]);
-% nbelow = sum(below);
-% X1 = [M1(below,1) Xfit(below,1) nan*ones(nbelow,1)];
-% X2 = [M1(below,2) Xfit(below,2) nan*ones(nbelow,1)];
-% X3 = [M1(below,3) Xfit(below,3) nan*ones(nbelow,1)];
+nbelow = sum(below);
+X1 = [M1(below,1) Xfit(below,1) nan*ones(nbelow,1)];
+X2 = [M1(below,2) Xfit(below,2) nan*ones(nbelow,1)];
+X3 = [M1(below,3) Xfit(below,3) nan*ones(nbelow,1)];
 % plot3(X1',X2',X3','-', M1(below,1),M1(below,2),M1(below,3),'o', 'MarkerSize', 1, 'Color',[1 0 0]);
-% 
+
 % hold off
-% maxlim = max(abs(M1(:)))*1.1;
+maxlim = max(abs(M1(:)))*1.1;
 % axis([0 maxlim -maxlim maxlim -0.5 1.8]);
 % axis square
 % view(-9,12);
-% Z_Loc = 0.1191;
+Z_Loc = 0.1191;
 for i = 1:682
     angle(i) = (i-1) * theta_res + min_ang;
 end
@@ -410,13 +426,13 @@ end
 %     end
 % end
 % range_mean = sum(range_zero)/nnz(range_zero);
-% 
+%
 % % cartesian coordinate of the point hit by the beam emanting from the front
 % % of the sensor center, essentially from the perpendicular direction
 % [x,y,z] = sph2cart(0, 0, range_mean);
-% 
+%
 % ax = atan2(norm(cross(normal,[x;y;z])),dot(normal,[x;y;z]));  % angle between the normal and the perpendicular range vector
-% 
+%
 % Z_off = 0.119;
 % pitch_off = 3.4;
 % mult = 1.0;
@@ -425,7 +441,7 @@ end
 % R1_angle = 1.4; R2_angle = 1.0648;
 % perp_1 = 0.02; perp_2 = 0.07;
 % L1_distance = 0.0587; L2_distance = 0.08224;
-% 
+%
 % aa=1;
 
 
@@ -848,9 +864,9 @@ for l = 1:900
 end
 
 for l = 1:900
-    range4((l-1) * 107 + 1: l * 107) = matR(l,570:676);
-    pitch_comp4((l-1) * 107 + 1: l * 107) = laser_pitch(l,570:676);
-    theta_comp4((l-1) * 107 + 1: l * 107) = angle(570:676);
+    range4((l-1) * 100 + 1: l * 100) = matR(l,570:669);
+    pitch_comp4((l-1) * 100 + 1: l * 100) = laser_pitch(l,570:669);
+    theta_comp4((l-1) * 100 + 1: l * 100) = angle(570:669);
 end
 
 
